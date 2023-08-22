1a8b52b1db11f2b1805e568beeac862106435af5
kadrikondi <abdulkadri42@gmail.com>
2023-08-22 17:16:13 +0100

    start nodejs

---
+++ app.js
@@ -0,0 +1,17 @@
+let name = 'idris'
+
+console.log(name)
+
+// norma; function
+function greet() {
+    console.log('welcome')
+}
+greet()
+
+
+// arrow function
+const greets = () => {
+    console.log('this is arroww fucntion')
+}
+greets()
+console.log(__filename)
---
+++ people.js
@@ -0,0 +1,11 @@
+const people = ['ola', 'john', 'muhammmed', 'josh', 'dan']
+const age = [23, 34, 56, 56, 7, 8, 5]
+
+// console.log(people)
+
+
+
+module.exports = {
+    people,
+    age
+}
